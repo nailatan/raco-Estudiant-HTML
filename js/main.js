6ad95c75f11846e0b0e9416b86cb3be1eb9384f5
Natalia <naguado@gmail.com>
2022-03-19 16:13:10 +0100

    Mejorar código Javascript de generación de la agenda

--- js/main.js
+++ js/main.js
@@ -35,113 +35,150 @@ function onClickClose(e) {
   popup.style.display = "none";
 }
 
-function obtenerPrimerDiaMes(mes, anyo) {
+const obtenerPrimerDiaMes = (mes, anyo) => {
   let primeroMes = new Date();
   primeroMes.setDate(1);
   primeroMes.setUTCFullYear(anyo);
   primeroMes.setMonth(mes);
 
   return primeroMes;
-}
+};
 
-function eliminarMesHtml() {
-  let bodyAgenda = document.querySelector("tbody");
+const generarMes = (mes, anyo) => {
+  const tablaAgenda = document.querySelector("table");
+  const bodyAgenda = tablaAgenda.querySelector("tbody");
 
-  while (bodyAgenda.firstChild) {
-    bodyAgenda.removeChild(bodyAgenda.firstChild);
-  }
-}
+  tablaAgenda.removeAttribute("x-data-index");
+  tablaAgenda.setAttribute("x-data-index", anyo);
 
-function generarMes(mes, anyo) {
-  let primeroMes = obtenerPrimerDiaMes(mes, anyo);
+  let diasMesGenerados = 0;
+  const primeroMes = obtenerPrimerDiaMes(mes, anyo);
 
-  let primerDiaSemana = convertGetUCTDay[primeroMes.getUTCDay()];
+  const eliminarMesHtml = () => {
+    bodyAgenda.textContent = "";
+  };
+
+  const generarPrimeraSemana = () => {
+    const primerDiaSemana = convertGetUCTDay[primeroMes.getUTCDay()];
+    let primeraSemana = document.createElement("tr");
+    let diaActual = 1;
+    for (let diaSemana = 0; diaSemana < 7; diaSemana++) {
+      if (diaSemana < primerDiaSemana) {
+        let nuevoDia = document.createElement("td");
+        primeraSemana.appendChild(nuevoDia);
+      } else {
+        let nuevoDia = document.createElement("td");
+        let nuevoDiaDiv = document.createElement("div");
+        nuevoDiaDiv.innerText = diaActual++;
+        nuevoDia.appendChild(nuevoDiaDiv);
+        primeraSemana.appendChild(nuevoDia);
+        diasMesGenerados++;
+      }
+    }
+    return primeraSemana;
+  };
+
+  // const generarRestoMes = () => {
+  //   let diasEnSemana = 0;
+  //   let semana;
+  //   for (
+  //     let diaActual = diasMesGenerados + 1;
+  //     diaActual <= totalDiasXMes[mes];
+  //     diaActual++
+  //   ) {
+  //     if (diasEnSemana === 0) {
+  //       semana = document.createElement("tr");
+  //     }
+  //     let nuevoDia = document.createElement("td");
+  //     let nuevoDiaDiv = document.createElement("div");
+  //     nuevoDiaDiv.innerText = diaActual;
+  //     nuevoDia.appendChild(nuevoDiaDiv);
+  //     semana.appendChild(nuevoDia);
+  //     diasEnSemana++;
+  //     if (diasEnSemana >= 7 || diaActual === totalDiasXMes[mes]) {
+  //       diasEnSemana = 0;
+  //       bodyAgenda.appendChild(semana);
+  //     }
+  //   }
+  // };
+
+  const generarRestoMes = () => {
+    let diasEnSemana = 0;
+    let semana;
+    let semanas = [];
+    for (
+      let diaActual = diasMesGenerados + 1;
+      diaActual <= totalDiasXMes[mes];
+      diaActual++
+    ) {
+      if (diasEnSemana === 0) {
+        semana = document.createElement("tr");
+      }
+      let nuevoDia = document.createElement("td");
+      let nuevoDiaDiv = document.createElement("div");
+      nuevoDiaDiv.innerText = diaActual;
+      nuevoDia.appendChild(nuevoDiaDiv);
+      semana.appendChild(nuevoDia);
+      diasEnSemana++;
+      if (diasEnSemana >= 7 || diaActual === totalDiasXMes[mes]) {
+        diasEnSemana = 0;
+        semanas.push(semana);
+      }
+    }
+    return semanas;
+  };
 
   eliminarMesHtml();
-  let tituloMes = document.querySelector("thead th[colspan='5'  ]");
+  let tituloMes = tablaAgenda.querySelector("thead th[colspan='5'  ]");
   tituloMes.innerText = meses[mes];
-  let bodyAgenda = document.querySelector("tbody");
 
-  let primeraSemana = document.createElement("tr");
-  let diasMesGenerados = 0;
-  let diaActual = 1;
-  for (let diaSemana = 0; diaSemana < 7; diaSemana++) {
-    if (diaSemana < primerDiaSemana) {
-      let nuevoDia = document.createElement("td");
-      primeraSemana.appendChild(nuevoDia);
-    } else {
-      let nuevoDia = document.createElement("td");
-      let nuevoDIaDiv = document.createElement("div");
-      nuevoDIaDiv.innerText = diaActual++;
-      nuevoDia.appendChild(nuevoDIaDiv);
-      primeraSemana.appendChild(nuevoDia);
-      diasMesGenerados++;
-    }
-  }
-  bodyAgenda.appendChild(primeraSemana);
+  bodyAgenda.appendChild(generarPrimeraSemana());
+  generarRestoMes().map((semana) => bodyAgenda.appendChild(semana));
 
-  let diasEnSemana = 0;
-  let semana;
-  for (
-    let diaActual = diasMesGenerados + 1;
-    diaActual <= totalDiasXMes[mes];
-    diaActual++
-  ) {
-    if (diasEnSemana === 0) {
-      semana = document.createElement("tr");
-    }
-    let nuevoDia = document.createElement("td");
-    let nuevoDIaDiv = document.createElement("div");
-    nuevoDIaDiv.innerText = diaActual;
-    nuevoDia.appendChild(nuevoDIaDiv);
-    semana.appendChild(nuevoDia);
-    diasEnSemana++;
-    if (diasEnSemana >= 7 || diaActual === totalDiasXMes[mes]) {
-      diasEnSemana = 0;
-      bodyAgenda.appendChild(semana);
-    }
-  }
-
+  // Evento click de cada uno de los dias de la agenda
   let dias = [...document.querySelectorAll("td div")];
 
   dias.map((elem) => {
     elem.addEventListener("click", abrirDetalleDia);
   });
-}
+};
 
 function cambiarMes(sentido) {
-  let tituloMes = document.querySelector("thead th[colspan='5'  ]");
-  let anyo = document.querySelector("#anyoAgenda");
+  const tablaAgenda = document.querySelector("table");
+
+  const tituloMes = tablaAgenda.querySelector("thead th[colspan='5'  ]");
+  let anyo = Number(tablaAgenda.getAttribute("x-data-index"));
+
   let mes = meses.indexOf(tituloMes.innerText);
   if (mes === 0 && sentido === "A") {
     mes = 11;
-    anyo.innerText = anyo.innerText - 1;
+    anyo--;
   } else if (mes === 11 && sentido === "S") {
-    anyo.innerText = Number(anyo.innerText) + 1;
+    anyo++;
     mes = 0;
   } else {
     "S" === sentido ? mes++ : mes--;
   }
-  generarMes(mes, anyo.innerText);
+  generarMes(mes, anyo);
 }
 
-//Generación agenda
-let agenda = document.querySelector("#contenedorAgenda");
-
+// Generación agenda
 let hoy = new Date();
 generarMes(hoy.getMonth(), hoy.getUTCFullYear());
 
-//Gestión de eventos de Mes anterior y posterior
-let MesAnterior = document.querySelector("img[src ='./images/arrow-left.svg']");
+// Gestión de eventos de mes anterior y posterior
+const MesAnterior = document.querySelector(
+  "img[src ='./images/arrow-left.svg']"
+);
 MesAnterior.addEventListener("click", () => cambiarMes("A"));
 
-let mesSiguiente = document.querySelector(
+const mesSiguiente = document.querySelector(
   "img[src ='./images/arrow-right.svg']"
 );
 mesSiguiente.addEventListener("click", () => cambiarMes("S"));
 
-//Gestión de evento para cerrar la ventana con el detalle de eventos de un dia
-let close = [...document.querySelectorAll("img[src='./images/close.svg']")];
+// Gestión de evento para cerrar el popup con el detalle de eventos de un dia
+const close = [...document.querySelectorAll("img[src='./images/close.svg']")];
 
 close.map((elem) => {
   elem.addEventListener("click", onClickClose);
